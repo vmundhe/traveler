3d1cf4efbad237b9ed59bb7e97ab65c679b585a0
Vishal Mundhe <vmundhe@Vishals-MacBook-Pro.local>
2017-07-07 22:55:44 -0400

    adding routers, controller for Auth, and editing User model

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout');
